bdc2a603e0da3f931e30012e00e6b50e8444ffac
orangeC <18332563755@163.com>
2016-10-16 22:01:45 +0800

    third

---
+++ js/fontsize.js
@@ -0,0 +1,1 @@
+ !function(a){function b(){var b=a.document,c=b.documentElement,d=c.getBoundingClientRect().width;document.documentElement.style.fontSize=100*(d/640)+"px"}window.addEventListener("DOMContentLoaded",function(){b()},!1),window.addEventListener("resize",function(){b()}),b()}(window);
